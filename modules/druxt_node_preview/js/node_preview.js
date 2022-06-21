b3214d2c322b95028f4f80539c96a9224d1407ff
Stuart Clark <stu@rtclark.net>
2022-06-21 12:23:19 +1000

    chore(#3264181): update styles and js

--- modules/druxt_node_preview/js/node_preview.js
+++ modules/druxt_node_preview/js/node_preview.js
@@ -1,16 +1,22 @@
 (function (Drupal, $) {
-    Drupal.behaviors.myTheme = {
-      attach: function attach(context) {
-        if (context === document) {
-          $(context).on('drupalViewportOffsetChange.toolbar', function (event, offsets) {
-            $('.druxt-node-preview iframe')
-              .css('top', offsets.top + parseInt($('.node-preview-container').css('height')))
-              .css('right', offsets.right)
-              .css('bottom', offsets.bottom)
-              .css('left', offsets.left)
-          });
-        }
+  Drupal.behaviors.myTheme = {
+    attach: function attach(context) {
+      if (context === document) {
+        $(context).on('drupalViewportOffsetChange.toolbar', function (event, offsets) {
+          var top = offsets.top + parseInt($('.node-preview-container').css('height'))
+          var right = offsets.right
+          var bottom = offsets.bottom
+          var left = offsets.left
+
+          $('.druxt-node-preview iframe')
+            .css('top', top)
+            .css('right', right)
+            .css('bottom', bottom)
+            .css('left', `var(--ginVerticalToolbarOffset, ${left}px)`)
+            .css('height', `calc(100vh - ${top}px - ${bottom}px)`)
+            .css('width', `calc(100vw - var(--ginVerticalToolbarOffset, ${left}px) - ${right}px)`)
+        });
       }
-    };
-  })(Drupal, jQuery)
-  
+    }
+  };
+})(Drupal, jQuery)
